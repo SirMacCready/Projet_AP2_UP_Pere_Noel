7f51a8771c82aa50dc820d26336db27f81f9a9de
unknown <cloe.biringer.tb@gmail.com>
2025-11-14 14:49:17 +0100

    Liste chainée, pile et file

---
+++ DataStructure.cs
@@ -0,0 +1,136 @@
+using System.Linq.Expressions;
+
+public class DataStructure
+{
+    public class LinkedList<T>
+    {
+        private Node<T>? _head;
+        private Node<T>? _tail;
+
+        public LinkedList()
+        {
+            this._head = null;
+            this._tail = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return this._head == null && this._tail == null;
+        
+        }
+
+        public void InsertAtFirst(T value)
+        {
+            if(this.IsEmpty())
+            {
+                this._head = new Node<T>(value, null);
+                this._tail = this._head;
+            }
+            else
+            {
+                this._head = new Node<T>(value, this._head);
+            }
+        }
+
+        public void InsertAtLast(T value)
+        {
+            if (this.IsEmpty())
+            {
+                InsertAtFirst(value);
+            }
+            else
+            {
+                Node<T> newNode = new Node<T>(value, null);
+                this._tail.Next = newNode;
+                this._tail = newNode;
+            }
+        }
+
+        public Node<T> RemoveAtFirst()
+        {
+            if (this.IsEmpty())
+            {
+                throw new ArgumentOutOfRangeException("La liste est vide");
+            }
+            Node<T> nodeToRemove = this._head;
+            if (this._head.Next == null)
+            {
+                this._head = null;
+                this._tail = null;
+            }
+            else
+            {
+                this._head = this._head.Next;
+            }
+            return nodeToRemove;
+        }
+
+    }
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        internal Node<T>? Next { get; set; }
+
+        public Node(T value, Node<T>? next)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+    }
+
+    public class Queue<T>
+    {
+        private LinkedList<T> LinkedListQueue;
+
+        public Queue()
+        {
+            this.LinkedListQueue = new LinkedList<T>();
+        }
+
+        public bool IsEmpty()
+        {
+            return this.LinkedListQueue.IsEmpty();
+        }
+
+        public void Enqueue(T value)
+        {
+            this.LinkedListQueue.InsertAtLast(value);
+        }
+
+        public Node<T> Dequeue()
+        {
+            return this.LinkedListQueue.RemoveAtFirst();
+        }
+
+    }
+
+    public class Stack<T>
+    {
+        private LinkedList<T> LinkedListStack;
+
+        public Stack()
+        {
+            this.LinkedListStack = new LinkedList<T>();
+        }
+        public bool IsEmpty()
+        {
+            return this.LinkedListStack.IsEmpty();
+        }
+
+        public void Push(T value)
+        {
+            this.LinkedListStack.InsertAtFirst(value);
+        }
+
+        public Node<T> Pop()
+        {
+            return this.LinkedListStack.RemoveAtFirst();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Node<int> n1 = new Node<int>(1, null);
+        Console.WriteLine(n1.Value);
+    }
+}
